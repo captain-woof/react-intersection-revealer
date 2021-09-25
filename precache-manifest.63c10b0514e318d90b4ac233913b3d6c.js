7437e092dd06e8dcd8efb51a56e3c9298a789f41
Captain Woof <captainwoof.official@gmail.com>
2021-09-26 01:50:19 +0530

    Updates

---
+++ precache-manifest.63c10b0514e318d90b4ac233913b3d6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d445971cb5fdc4c69ace366cca584538",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6d73b4e6e6579f5ee38a",
+    "url": "./static/js/2.e1d632dd.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.e1d632dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2319b26a2513446dc8a",
+    "url": "./static/js/main.e197800d.chunk.js"
+  },
+  {
+    "revision": "34553b5a73bdc297f869",
+    "url": "./static/js/runtime-main.4d9473dd.js"
+  }
+]);
